0deec8fdf83641b500dab2eff3a2fc0cfa335d12
sulthoni12345 <sulthoniprima123@gmail.com>
2026-04-23 23:04:23 +0700

    Integrator-A-Sulthoni

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::post('/booking/create', [BookingController::class, 'store']);
