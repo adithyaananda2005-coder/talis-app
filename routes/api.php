95b09591f07bb9f7038225e85501707ac8247042
azrielreynard <azrielreynardd@gmail.com>
2026-04-24 17:45:54 +0700

    Setup AssetController.php, input data paket dari katalog logistik (Paket Aula, Listrik, Streaming, Komunikasi)

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AssetController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::get('/assets/package/{nama_paket}',
+           [AssetController::class, 'getDetail']);
+
