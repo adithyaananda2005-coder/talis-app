dbc87d4ffaf8950f1131b2e4e119fc36c81a384e
adithyaananda2005-coder <adithyaananda2005@gmail.com>
2026-04-26 13:49:09 +0700

    update booking migration & seeder

--- database/migrations/2026_04_23_131340_create_bookings_table.php
+++ database/migrations/2026_04_23_131340_create_bookings_table.php
@@ -6,15 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-  public function up(): void
-{
-    Schema::create('bookings', function (Blueprint $table) {
-        $table->id();
-        $table->string('nama');
-        $table->string('nim');
-        $table->string('nama_paket');
-        $table->string('status')->default('pending');
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_paket');
+            $table->string('key_paket')->unique(); // aula, streaming, listrik, komunikasi
+            $table->string('status')->default('tersedia'); // tersedia / diperbaiki
+            $table->timestamps();
+        });
+
+        Schema::create('asset_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('asset_id')->constrained('assets')->onDelete('cascade');
+            $table->string('nama_barang');
+            $table->integer('qty');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('asset_items');
+        Schema::dropIfExists('assets');
+    }
 };
